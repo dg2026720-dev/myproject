2c5c47196d328e7f40cdef1920dd216818bcc335
dg2026720-dev <dg2026720@danggok.hs.kr>
2026-05-19 09:10:45 +0900

    Create main.py
    시뮬레이션이 진행될 박스를 만들고 체크박스를 만들었다

---
+++ main.py
@@ -0,0 +1,41 @@
+box(size = vec(1,130,40) , pos = vec(80,10,0))
+box(size = vec(1,130,40) , pos = vec(-80,10,0))
+box(size = vec(160,130,1) , pos= vec(0,10,-20))
+box(size = vec(160,1,40), pos = vec(0,-55,0))
+box(size = vec(160,130,1) , pos= vec(0,10,20),opacity = 0)
+
+def checkbox_event(checkbox):
+    if checkbox.checked:
+        if checkbox.text == 'NaOH':
+            NaOH.visible = True
+        elif checkbox.text == 'KOH':
+            KOH.visible = True
+        elif checkbox.text == 'NA4OH':
+            NA4OH.visible = True
+        elif checkbox.text == 'Ca(OH)':
+            Ca(OH).visible = True
+        elif checkbox.text == 'HCL':
+            HCL.visible = True
+        elif checkbox.text == 'HNO3':
+            HNO3.visible = True
+        elif checkbox.text == 'H2SO4':
+            H2SO4.visible = True
+        elif checkbox.text == 'H2CO3':
+            H2CO3.visible = True
+    else:
+        if checkbox.text == 'NaOH':
+            NaOH.visible = False
+        elif checkbox.text == 'KOH':
+            KOH.visible = False
+        elif checkbox.text == 'NA4OH':
+            NA4OH.visible = False
+        elif checkbox.text == 'Ca(OH)':
+            Ca(OH).visible = False
+        elif checkbox.text == 'HCL':
+            HCL.visible = False
+        elif checkbox.text == 'HNO3':
+            HNO3.visible = False
+        elif checkbox.text == 'H2SO4':
+            H2SO4.visible = False
+        elif checkbox.text == 'H2CO3':
+            H2CO3.visible = False
